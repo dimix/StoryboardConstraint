fe0a95a8df5c546914c00ed030982a666c8de4db
Dimitri Giani <dimitri.giani@gmail.com>
2017-02-21 21:54:17 +0100

    Now you can get all the constraints with a specific attribute ordered by priority. Added a little code documentation.

--- StoryboardConstraint/UIView+StoryboardConstraint.swift
+++ StoryboardConstraint/UIView+StoryboardConstraint.swift
@@ -24,6 +24,16 @@ extension UIView
 	var centerXConstraint:NSLayoutConstraint? { return superview?.constraint(withAttribute: .centerX, firstItem: self) }
 	var centerYConstraint:NSLayoutConstraint? { return superview?.constraint(withAttribute: .centerY, firstItem: self) }
 	
+	/**
+	Get the first constraints found with a specified identifier and with the specified first or second item set
+	- parameters:
+		- identifier: a String value that identifies a constraint set in Interface Builder
+		- firstItem: optional the item set as First
+		- secondItem: optional the item set as Second
+		- searchInSubviews: a Bool value, indicates if search through the subviews
+	- returns:
+		An optional NSLayoutConstraint
+	*/
 	public func constraint(withIdentifier identifier:String, firstItem: UIView? = nil, secondItem: UIView? = nil, searchInSubviews:Bool = false) -> NSLayoutConstraint?
 	{
 		var constraint:NSLayoutConstraint?
@@ -75,9 +85,48 @@ extension UIView
 		return constraint
 	}
 	
+	/**
+	Get the first constraints found with a specified attribute and with the specified first or second item set
+	- parameters:
+		- attribute: a NSLayoutAttribute value. Supported: top, bottom, leading, trailing, width, height, centerX, centerY
+		- firstItem: optional the item set as First
+		- secondItem: optional the item set as Second
+		- searchInSubviews: a Bool value, indicates if search through the subviews
+	- returns:
+		An optional NSLayoutConstraint
+	*/
 	public func constraint(withAttribute attribute: NSLayoutAttribute, firstItem: UIView? = nil, secondItem: UIView? = nil, searchInSubviews:Bool = false) -> NSLayoutConstraint?
 	{
-		var constraint:NSLayoutConstraint?
+		if let constraint = constraints(withAttribute: attribute, firstItem: firstItem, secondItem: secondItem).first
+		{
+			return constraint
+		}
+		else if searchInSubviews
+		{
+			for subview in subviews
+			{
+				if let aConstraint = subview.constraint(withAttribute: attribute, firstItem: firstItem, secondItem: secondItem, searchInSubviews: searchInSubviews)
+				{
+					return aConstraint
+				}
+			}
+		}
+		
+		return nil
+	}
+	
+	/**
+	Get all the constraints with a specified attribute and with the specified first or second item set
+	- parameters:
+		- attribute: a NSLayoutAttribute value. Supported: top, bottom, leading, trailing, width, height, centerX, centerY
+		- firstItem: optional the item set as First
+		- secondItem: optional the item set as Second
+	- returns:
+		An array of NSLayoutConstraint
+	*/
+	public func constraints(withAttribute attribute: NSLayoutAttribute, firstItem: UIView? = nil, secondItem: UIView? = nil) -> [NSLayoutConstraint]
+	{
+		var foundConstraints:[NSLayoutConstraint] = []
 		
 		for aConstraint in constraints
 		{
@@ -87,42 +136,26 @@ extension UIView
 				{
 					if firstItem == (aConstraint.firstItem as? UIView)
 					{
-						constraint = aConstraint
+						foundConstraints.append(aConstraint)
 					}
 				}
 				else if let secondItem = secondItem
 				{
 					if secondItem == (aConstraint.secondItem as? UIView)
 					{
-						constraint = aConstraint
+						foundConstraints.append(aConstraint)
 					}
 				}
 				else
 				{
 					if aConstraint.firstAttribute == attribute
 					{
-						constraint = aConstraint
+						foundConstraints.append(aConstraint)
 					}
 				}
-				
-				if constraint != nil
-				{
-					break
-				}
 			}
 		}
 		
-		//	If requested and the constraint isn't find, search through the subviews
-		
-		if constraint == nil && searchInSubviews
-		{
-			for subview in subviews
-			{
-				constraint = subview.constraint(withAttribute: attribute, firstItem: firstItem, secondItem: secondItem, searchInSubviews: searchInSubviews)
-				if constraint != nil { break }
-			}
-		}
-		
-		return constraint
+		return foundConstraints.sorted(by: { $0.priority > $1.priority })
 	}
 }
